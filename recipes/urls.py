a201e84e06b096680896bce4f57273f0a40d78cf
J-VictorJ <padrinhoreserva@gmail.com>
2025-09-24 16:23:13 -0300

    using "include". adding namespace. creating head.html and header.html at "partials" folder

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.urls import path
-from recipes.views import home, sobre
+from recipes.views import home
 
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
+    #path('sobre/', sobre),
     # The 'sobre' path will return a simple HTTP response when accessed
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Importing the HttpResponse function to return a simple HTTP response
 #But in recipes.views
 
 def home(request):
-    return render(request, 'home.html', { 'name': 'я Виктор'})
+    return render(request, 'recipes/pages/home.html', context={ 'name': 'я Виктор'})
 
-def sobre(request):
-    return HttpResponse("\"Sobre\" a página de receitas")
+#def sobre(request):
+#    return HttpResponse("\"Sobre\" a página de receitas")
     # return render(request, 'recipes/sobre.html')
     # This will render the 'sobre.html' template located in the 'recipes' app's templates directory
