18ecd4b20e075eff08d7bd62bfe9486a499e2c9f
J-victorj <padrinhoreserva@gmail.com>
2026-05-23 18:57:42 -0300

    "Creating and finishing by now tests, unittest, using pytest/unittest"

---
+++ recipes/tests/test_recipe_model_category.py
@@ -0,0 +1,23 @@
+from .test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError
+
+
+class RecipeCategoryModelTest(RecipeTestBase):
+    def setUp(self):
+        self.category = self.make_category(
+            name='Category Testing'
+        )
+        return super().setUp()
+    
+    
+    def test_recipe_category_model_str_representation_is_name_field(self):
+        self.assertEqual(
+            str(self.category),
+            self.category.name
+        )
+        
+        
+    def test_recipe_category_model_name_max_len_is_67_chars(self):
+        self.category.name = "67" * 64
+        with self.assertRaises(ValidationError):
+            self.category.full_clean()
